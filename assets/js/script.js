56e58d50dbd4514b1a1c111908a58d6b566be9d9
Reht <reht.dev@gmail.com>
2021-12-01 17:01:20 -0600

    commenting html/javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//global variables
 var now = moment().format('dddd, MMMM Do, YYYY');
 var currentTime = moment().hours()
 var timeBlock = $('.time-block')
@@ -6,7 +7,8 @@ var saveBtn = $(".saveBtn")
 $("#currentDay").text(now)
 
 setColor()
-
+//this function designates the color of each timeblock section in accordance with the past, present, future classes
+//cycles through each timeBlock setting the id from the html = var hour, compares it to the currentTime, adds/removes style classes as indicated 
 function setColor() {
     timeBlock.each(function() {
         var hour = $(this).attr('id')
@@ -25,6 +27,8 @@ function setColor() {
     })
 }
 
+//eventlistner selects the "textarea" sibling to the clicked button assigning the key = "id" carried from the html and the value = "task" which is the user text input for that row
+//it commits the id and task to local storage
 saveBtn.on('click', function() {
     var id = $(this).attr('id')
     var task = $(this).siblings('textarea').val()
@@ -34,6 +38,7 @@ saveBtn.on('click', function() {
 
 getTasks()
 
+//
 function getTasks() {
     for (var i = 9; i < 18; i++) {
         var currentTask = localStorage.getItem(i)
